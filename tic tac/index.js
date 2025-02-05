7a55f6d4497d97a5ada78b18c9a2d0072773a704
24LP6734 24LP6734 <24LP6734@G401-CC0116.local>
2025-02-05 15:30:05 +0800

    tic tac toe done

--- tic tac/index.js
+++ tic tac/index.js
@@ -1,22 +1,8 @@
 const board = document.querySelectorAll(".inner");
 const clear = document.querySelector("#clearButton");
 let clicktimes = 0;
-for (let i = 0; i < board.length; i++) {
-  board[i].addEventListener("click", () => {
-    let value = board[i].innerHTML;
-    if (value === "") {
-      board[i].innerHTML = "X";
-      clicktimes++;
-      if ((clicktimes > 1) & (clicktimes % 2 === 0)) {
-        board[i].innerHTML = "O";
-      }
-    }
-  });
-  clear.addEventListener("click", () => {
-    value = "";
-    board[i].innerHTML = value;
-  });
-}
+let gameover = false;
+
 function checkWinner() {
   const winningPostion = [
     [0, 1, 2],
@@ -28,13 +14,63 @@ function checkWinner() {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let j = 0; j < winningPostion.length; j++) {
+  for (let i = 0; i < winningPostion.length; i++) {
+    const combo = winningPostion[i];
+    const [a, b, c] = combo;
+    const firstCell = board[a].innerHTML;
+    const secondCell = board[b].innerHTML;
+    const thirdCell = board[c].innerHTML;
     if (
-      (board[j][0].innerHTML = "X") &&
-      (board[j][1].innerHTML = "X") &&
-      (board[j][2].innerHTML = "X")
+      firstCell === secondCell &&
+      secondCell === thirdCell &&
+      secondCell !== ""
     ) {
+      return firstCell;
     }
-    console.log(board[j][0]);
+  }
+  return null;
+}
+
+function displayMessage(winner) {
+  const message = document.getElementById("message");
+  if (winner) {
+    message.innerHTML = `Player ${winner} wins!`;
+  } else if (clicktimes === 9) {
+    message.innerHTML = "It's a tie!";
+  } else {
+    message.innerHTML = "";
   }
 }
+
+for (let i = 0; i < board.length; i++) {
+  board[i].addEventListener("click", () => {
+    if (gameover) return;
+
+    let value = board[i].innerHTML;
+    if (value === "") {
+      if (clicktimes % 2 === 0) {
+        board[i].innerHTML = "X";
+      } else {
+        board[i].innerHTML = "O";
+      }
+      clicktimes++;
+
+      const winner = checkWinner();
+      if (winner) {
+        displayMessage(winner);
+        gameover = true;
+      } else {
+        displayMessage();
+      }
+    }
+  });
+}
+
+clear.addEventListener("click", () => {
+  for (let i = 0; i < board.length; i++) {
+    board[i].innerHTML = "";
+  }
+  clicktimes = 0;
+  gameover = false;
+  displayMessage();
+});
